de6eb7b9aa75ea778968c38a7f3ad9eef90de8eb
Selim Yaman <mselimyaman@gmail.com>
2022-10-04 22:33:00 +0300

    footer güncellendi 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import YGlogo from "../img/ygtnavbar.svg";
+import YGlogo from "../img/ygtLogo.svg";
 import KTlogo from "../img/ktunlogo.png";
-import insta from "../img/square-instagram.svg";
-import twitter from "../img/twitter.svg";
-import linkedin from "../img/linkedin.svg";
-import discord from "../img/discord.svg";
+import insta from "../img/instagram-clr.png";
+import twitter from "../img/twitter-clr.png";
+import linkedin from "../img/linkedin-clr.png";
+import discord from "../img/discord-clr.png";
 
 function Footer() {
+    
   return (
     <section>
       <div className="footer-box">
@@ -44,41 +45,41 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="flogo-box">
           <img alt="yg-logo" className="footer-logo" src={YGlogo} />
         </div>
-        <div>
+        <div className="flogo-box">
           <img alt="ktun-logo" className="footer-logo" src={KTlogo} />
         </div>
         <div className="footer-socials">
-          <h3>Bizi Takip Edin!</h3>
+          <h3 className="f-social-title">Bizi Takip Edin!</h3>
           <ul className="footer-list">
             <li list>
-              <a className="footer-list-item" href="#">
-                <img className="social-logo" alt="insta-logo" src={insta} />{" "}
-                @ktun.yazilimgelistirme
+              <a className="footer-list-item" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ktun.yazilimgelistirme/">
+                <span className="f-social">Instagram</span>{" "}
+                <img className="social-logo" alt="insta-logo" src={insta} />
               </a>
             </li>
             <li>
-              <a className="footer-list-item" href="#">
-                <img className="social-logo" alt="twitter-logo" src={twitter} />{" "}
-                @KTUN.YazilimGT
+              <a className="footer-list-item" target="_blank" rel="noopener noreferrer" href="https://twitter.com/KTUNYazilimGT">
+                <span className="f-social">Twitter</span>{" "}
+                <img className="social-logo" alt="twitter-logo" src={twitter} />
               </a>
             </li>
             <li>
-              <a className="footer-list-item" href="#">
+              <a className="footer-list-item" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/ktun-yazilimgelistirme/">
+                <span className="f-social">LinkedIn</span>{" "}
                 <img
                   className="social-logo"
                   alt="linkedin-logo"
                   src={linkedin}
-                />{" "}
-                @ktun.yazilimgelistirme
+                />
               </a>
             </li>
             <li>
-              <a className="footer-list-item" href="#">
-                <img className="social-logo" alt="discord-logo" src={discord} />{" "}
-                Yazılım Geliştirme Topluluğu
+              <a className="footer-list-item" target="_blank" rel="noopener noreferrer" href="http://discord.gg/NgKXpsyatr">
+                <span className="f-social">Discord</span>{" "}
+                <img className="social-logo" alt="discord-logo" src={discord} />
               </a>
             </li>
           </ul>
